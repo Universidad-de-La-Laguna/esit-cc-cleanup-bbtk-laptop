88a6c6b60a32bff666432a2dd09c4ededdb86589
Benito Cuesta Viera <benitojcv@iMac-de-Benito.local>
2020-06-10 13:13:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# esit-cc-cleanup-bbtk-laptop
+
+Script para la limpieza de portátiles de BBTK preparados para teletrabajo.
+
+*Pendiente escribir documentación de uso*
---
+++ bbtk_cleanup.ps1
@@ -0,0 +1,40 @@
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+<#
+.SYNOPSIS
+  Script para la limpieza de equipos de BBTK para teletrabajo
+.LINK
+  xxx
+#>
+# Leemos la cuenta actual
+$cuenta = [Environment]::UserName
+
+# Pregunta de confirmacion
+Write-Warning "Este programa eliminará la cuenta $cuenta con la que ha iniciado sesión y creará una nueva con el mismo nombre. Esto provocará la pérdida de los datos contenidos en la cuenta de dicho usuario." -WarningAction Inquire
+
+# Renombramos la cuenta actual
+Write-Host "Renombrando cuenta $cuenta ..."
+$borrar = "borrar$(Get-Date -Format 'yymmddhhmmss')"
+Rename-LocalUser -Name "$cuenta" -NewName "$borrar"
+Write-Host "Cuenta $cuenta renombrada a $borrar"
+
+# Creamos de nuevo la cuenta
+Write-Host "Creando la cuenta $cuenta ..."
+$newpassword = ConvertTo-SecureString "usuario" -AsPlainText -Force
+New-LocalUser -Name "$cuenta" -Description "Cuenta generica de usuario" -Password $newpassword -PasswordNeverExpires -UserMayNotChangePassword -AccountNeverExpires
+Write-Host "Cuenta Usuario creada correctamente"
+
+# Añadir la cuenta al grupo de administradores
+Write-Host "Añadiendo la nueva cuenta al grupo de administradores..."
+Add-LocalGroupMember -Group "Administradores" -Member "$cuenta"
+Write-Host "Cuenta $cuenta añadida al grupo de administradores"
+
+# Eliminamos la cuenta antigua
+Write-Host "Eliminando cuenta $borrar ..."
+Remove-LocalUser -Name "$borrar"
+Write-Host "Cuenta $borrar eliminada correctamente"
+
+Write-Host "Este script ha terminado. En 5 segundos se cerrará la sesión actual"
+Start-Sleep -s 5
+logoff
