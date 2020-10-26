1bf0d27dfeed3822327b6cd778e3e65a4bc900a3
Benito Cuesta <bcuestav@ull.edu.es>
2020-10-26 11:03:30 +0000

    fix: Usar path del script en lugar de rutas estaticas

--- configure_ull_conecta.bat
+++ configure_ull_conecta.bat
@@ -1,3 +1,5 @@
-netsh wlan add profile filename=d:\ULL-CONECTA.xml user=all
-netsh wlan add profile filename=d:\Wi-Fi-eduroam.xml user=all
+@echo on
+SET mypath=%~dp0
+netsh wlan add profile filename=%mypath:~0,-1%\ULL-CONECTA.xml user=all
+netsh wlan add profile filename=%mypath:~0,-1%\Wi-Fi-eduroam.xml user=all
 pause
